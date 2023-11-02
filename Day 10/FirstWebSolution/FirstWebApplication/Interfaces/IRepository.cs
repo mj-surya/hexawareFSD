bfd0c43ddcd76052d48bbd9bf245b0b2c58f8cb0
mj-surya <mj7surya@gmail.com>
2023-11-02 17:50:24 +0530

    Day 10

---
+++ Day 10/FirstWebSolution/FirstWebApplication/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿
+using FirstWebApplication.Models;
+
+namespace FirstWebApplication.Interfaces
+{
+    public interface IRepository<K, T>
+        where T : class
+    {
+        public T Add(T item);
+        public T Delete(K key);
+        public T Get(K key);
+        public IList<T> GetAll();
+        public T Update(T item);
+    }
+}
