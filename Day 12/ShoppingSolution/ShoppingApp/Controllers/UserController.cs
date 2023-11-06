fcd1f07fb6f8883a3dc6fd1fd982ddaddafa55ec
mj-surya <mj7surya@gmail.com>
2023-11-06 17:38:17 +0530

    Day 12

---
+++ Day 12/ShoppingSolution/ShoppingApp/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Interfaces;
+using ShoppingApp.Models;
+using ShoppingApp.Models.DTOs;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingApp.Controllers
+{
+    
+    public class UserController : Controller
+    {
+        IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserVewModel viewmodel)
+        {
+            var result = _userService.Register(viewmodel);
+            if(result != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(UserVewModel viewmodel)
+        {
+            var result = _userService.Login(viewmodel);
+            if(result != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ModelState.AddModelError(string.Empty, "The user name or password is incorrect");
+            return View();
+        }
+    }
+}
---
+++ Day 12/ShoppingSolution/ShoppingApp/Interfaces/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using ShoppingApp.Models.DTOs;
+
+namespace ShoppingApp.Interfaces
+{
+    public interface IUserService
+    {
+        UserVewModel Login(UserVewModel userDTO);
+        UserVewModel Register(UserVewModel userDTO);
+    }
+}
